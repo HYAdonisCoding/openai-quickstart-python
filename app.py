687427bd60ee17a2537f58aa52f53aae067802e5
Eason <296786475@qq.com>
2023-02-10 17:41:05 +0800

    test openai code.

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
-import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+import requests
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route('/')
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+    return render_template('index.html')
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_text = request.form['text']
+    api_key = "YOUR_API_KEY"
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    params = {
+        "prompt": user_text,
+        "max_tokens": 100
+    }
+    response = requests.get(
+        "https://api.openai.com/v1/engines/text-davinci-002/completions",
+        headers=headers,
+        params=params
     )
+    if response.status_code == 200:
+        response_text = response.json()['choices'][0]['text']
+        return render_template('index.html', response_text=response_text)
+    else:
+        return render_template('index.html', response_text="Sorry, I am unable to respond at the moment.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
